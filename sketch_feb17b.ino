a295b2be1076d9bcfbd6ef61da0a911997a90b44
Chttan <s.dlsytjy@gmail.com>
2018-02-18 02:01:04 -0500

    Add files via upload
    - Added features
       The alarm light is flashing when it gets triggered.
       The alarm light is only triggered with an appropriate gesture and a loud sound.

--- sketch_feb17b.ino
+++ sketch_feb17b.ino
@@ -4,7 +4,6 @@
 #define GES_REACTION_TIME    400       // You can adjust the reaction time according to the actual circumstance.
 #define GES_ENTRY_TIME      800       // When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s). 
 #define GES_QUIT_TIME     1000
-#define GES_QUIT_TIME2   100
 
 int LED = 4;
 int LED2 = 2;
@@ -37,27 +36,65 @@ void setup()
 
 void loop()
 {
-    uint8_t data = 0, data1 = 0, error; 
-    static int flag=0; 
+    uint8_t data = 0, data1 = 0, data2=0, data3=0,error; 
+    static int flag=0, a=0; 
     
     error = paj7620ReadReg(0x43, 1, &data);       // Read Bank_0_Reg_0x43/0x44 for gesture result.
     
 loopA: 
     if(!error){
-      delay(GES_ENTRY_TIME);
-      if (data == GES_LEFT_FLAG){
+//preset password
+      delay(1000);
+      paj7620ReadReg(0x43, 1, &data1);
+      delay(10000);
+      Serial.println("Please input your gesture2:\n");
+      paj7620ReadReg(0x43, 1, &data2);
+      delay(10000);
+      Serial.println("Please input your gesture3:\n");
+      paj7620ReadReg(0x43, 1, &data3);
+
+//match the preset password
+//      if(data1 == GES_LEFT_FLAG){
+//        data1 = GES_LEFT_FLAG;
+//        }
+//        else if (data1 == GES_RIGHT_FLAG){
+//          data1 = GES_RIGHT_FLAG;
+//          }
+//     
+      if(data2 == GES_LEFT_FLAG){
+        a = 0;
+        }
+        else if (data2 == GES_RIGHT_FLAG){
+          a = 1;
+          }
+       
+//       if(data3 == GES_LEFT_FLAG){
+//        data3 = GES_LEFT_FLAG;
+//        }
+//        else if (data3 == GES_RIGHT_FLAG){
+//          data3 = GES_RIGHT_FLAG;
+//          }
+          
+      delay(100);
+      Serial.println("Start");
+       paj7620ReadReg(0x43, 1, &data);
+      if (data == data1){
         Serial.println("Left");
-        delay(20);
+        delay(50);
         paj7620ReadReg(0x43, 1, &data);
         switch(data)
         {
+ tryright:
           case GES_RIGHT_FLAG:
+            if(a==0){
+              goto tryleft;
+              }
             delay(GES_ENTRY_TIME);
             Serial.println("Right");
             flag = flag+1;
            
             paj7620ReadReg(0x43, 1, &data);
-            if(data == GES_LEFT_FLAG)
+            if(data == data3)
             {
                 delay(GES_ENTRY_TIME);
                 Serial.println("Forward");
@@ -71,8 +108,33 @@ loopA:
               goto loopA;            
               }
               break;
+ tryleft:             
+          case GES_LEFT_FLAG:
+            if(a==1){
+              goto tryright;
+              }
+            delay(GES_ENTRY_TIME);
+            Serial.println("Right");
+            flag = flag+1;
+           
+            paj7620ReadReg(0x43, 1, &data);
+            if(data == data3)
+            {
+                delay(GES_ENTRY_TIME);
+                Serial.println("Forward");
+                flag= flag+1;
+                
+              }
+             else
+             {
+              flag = 0;
+              Serial.println("Restart");
+              goto loopA;            
+              }
+              break;
+              
           default:
-            paj7620ReadReg(0x44, 1, &data1);   
+            paj7620ReadReg(0x44, 1, &data);   
              Serial.println("restart");
              flag=0;
              goto loopA;
@@ -118,5 +180,22 @@ loopA:
         }
  
     } 
+
+    int red, green, blue;
+    GroveColorSensor colorSensor;
+    colorSensor.ledStatus = 1;            // When turn on the color sensor LED, ledStatus = 1; When turn off the color sensor LED, ledStatus = 0.
+    while(1)
+    {
+        colorSensor.readRGB(&red, &green, &blue);    //Read RGB values to variables.
+        delay(300);
+        Serial.print("The RGB value are: RGB( ");
+        Serial.print(red,DEC);
+        Serial.print(", ");
+        Serial.print(green,DEC);
+        Serial.print(", ");
+        Serial.print(blue,DEC);
+        Serial.println(" )");
+        colorSensor.clearInterrupt();
+    }
     
 }
